0a711e313b509b42f8475ec3c31b494d8235db01
SJLynch94 <43658651+SJLynch94@users.noreply.github.com>
2019-05-27 19:39:19 +0100

    Update
    Updated Health Component so we don't update the Health Property value as that is done in the delegate function of Get Health

--- UtilityAIProto/Assets/Scripts/Components/HealthComponent.cs
+++ UtilityAIProto/Assets/Scripts/Components/HealthComponent.cs
@@ -29,18 +29,18 @@ public class HealthComponent : MonoBehaviour
     {
         Health += h;
         Health = Mathf.Clamp(Health, mMinHealth, mMaxHealth);
-        //if (Health >= 75 && Health <= mMaxHealth)
-        //{
-        //    HealthState = EHealthState.Healthy;
-        //}
-        //if (Health <= 74 && Health >= 40)
-        //{
-        //    HealthState = EHealthState.Injured;
-        //}
-        //if (Health <= 39 && Health >= 1)
-        //{
-        //    HealthState = EHealthState.Severe;
-        //}
+        if (Health >= 75 && Health <= mMaxHealth)
+        {
+            HealthState = EHealthState.Healthy;
+        }
+        if (Health <= 74 && Health >= 40)
+        {
+            HealthState = EHealthState.Injured;
+        }
+        if (Health <= 39 && Health >= 1)
+        {
+            HealthState = EHealthState.Severe;
+        }
     }
 
     public void Damage(int d)
@@ -51,17 +51,14 @@ public class HealthComponent : MonoBehaviour
             if(Health >= 75 && Health <= mMaxHealth)
             {
                 HealthState = EHealthState.Healthy;
-                //mAgent.mHealth.Value += 10.0f * UtilityAIProto.UAI_Time.MyTime;
             }
             if (Health <= 74 && Health >= 40)
             {
                 HealthState = EHealthState.Injured;
-                //mAgent.mHealth.Value += 25.0f * UtilityAIProto.UAI_Time.MyTime;
             }
             if(Health <= 39 && Health >= 1)
             {
                 HealthState = EHealthState.Severe;
-                //mAgent.mHealth.Value += 65.0f * UtilityAIProto.UAI_Time.MyTime;
             }
             if (Health <= 0)
             {
